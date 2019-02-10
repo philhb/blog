9d2d124ef950404b5ff3004c4349e3eca7386635
Phil Bennett <phil.bennett@outlook.com>
2019-02-10 23:07:46 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,1 @@
-var store = [{
-        "title": "First blog post",
-        "excerpt":"A blog post   A blog post   A blog post   A blog post   A blog post   A blog post  ","categories": [],
-        "tags": [],
-        "url": "http://www.philbennett.me/2019/02/10/First-blog-post.html",
-        "teaser":null}]
+var store = []
