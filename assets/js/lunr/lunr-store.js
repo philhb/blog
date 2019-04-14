2f42efeca71190318188e01b735f1fc3ce00fb0e
Phil Bennett <phil.bennett@outlook.com>
2019-04-14 22:05:14 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "Running a modern slavery helpline",
         "excerpt":"Over the last few years I’ve come to understand the importance of a national anti-slavery or human trafficking helpline. I sometimes worry there is sometimes a lack of awareness of what it really takes to run a busy helpline responsibly, and also why a helpline is so important. I’m going...","categories": [],
         "tags": [],
-        "url": "http://www.philbennett.me/year-archive/modern%20slavery%20helpline",
+        "url": "http://www.philbennett.me/year-archive/modern-slavery-helpline",
         "teaser":null}]
