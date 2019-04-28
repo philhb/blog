967c1da4891f7f83fbf975e40c05e0753a8e3795
Phil Bennett <phil.bennett@outlook.com>
2019-04-28 17:02:45 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Modern slavery helplines: what's involved?",
-        "excerpt":"Over the last few years I’ve come to understand the importance of a national modern slavery / human trafficking helpline. I sometimes worry there is a lack of awareness of what it really takes to run a busy helpline responsibly and why a helpline is so important. I will to...","categories": [],
+        "excerpt":"Over the last few years I’ve come to understand the importance of a national modern slavery helpline. There are now several across the world and they’ve become vital services for potential victims, members of the public and organisations. But I sometimes worry there is a lack of awareness of what...","categories": [],
         "tags": [],
         "url": "http://www.philbennett.me/modern-slavery-helplines",
         "teaser":null}]
