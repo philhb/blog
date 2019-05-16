24ca62a9bfc64ecd159a9cd5d5b74484e8a97828
Phil Bennett <phil.bennett@outlook.com>
2019-05-16 21:13:39 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -10,7 +10,7 @@ var store = [{
         "url": "http://www.philbennett.me/volunteer-technologists",
         "teaser":null},{
         "title": "Moonshot: an approach to cascade ethical behaviour in supply chains",
-        "excerpt":"I label some ideas I have as “moonshots”. In my mind, moonshots might be difficult but potentially enormously impactful ideas. Of course, there is a risk of being perceived as naive or ludicrously optimistic by the establishment. But I think it’s worth the reputational risk of sharing an idea which...","categories": [],
+        "excerpt":"I label some of my ideas as “moonshots”. In my mind, moonshots might be difficult but potentially enormously impactful ideas. Of course, there is a risk of being perceived as naive or ludicrously optimistic by the establishment. But I think it’s worth the reputational risk of sharing an idea which...","categories": [],
         "tags": [],
         "url": "http://www.philbennett.me/cei",
         "teaser":null}]
