5c51daea60f3e1ef58f94b0b40e6131c18254837
Phil Bennett <phil.bennett@outlook.com>
2019-05-16 20:00:50 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,4 +8,9 @@ var store = [{
         "excerpt":"As a technologist you’re probably acutely aware of the power and potential for technologies to help organisations transform and make them more impactful. As a would-be volunteer you’ve become passionate about “giving back” and want to use your valuable skills in a meaningful way. Surprisingly, the path to successfully volunteering...","categories": [],
         "tags": [],
         "url": "http://www.philbennett.me/volunteer-technologists",
+        "teaser":null},{
+        "title": "Moonshot: a market Driven approach to cascade ethical behaviour in supply chains",
+        "excerpt":"The fact that so many companies in the world are connected in some way to each other via a complex network of commercial relationships is tantalising to someone looking for high-scale solutions to tackle slavery and labour exploitation in supply chains. The intricate and ever-changing web of interactions and relationships...","categories": [],
+        "tags": [],
+        "url": "http://www.philbennett.me/cei",
         "teaser":null}]
