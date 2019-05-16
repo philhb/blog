75a3a1179771243478240daf6b68db545bb45ea1
Phil Bennett <phil.bennett@outlook.com>
2019-05-16 22:02:21 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,11 +4,6 @@ var store = [{
         "tags": [],
         "url": "http://www.philbennett.me/modern-slavery-helplines",
         "teaser":null},{
-        "title": "National modern slavery helplines: what's involved?",
-        "excerpt":"Over the last few years I’ve come to understand the importance of a national modern slavery helpline. There are now several across the world and they’ve become vital services for potential victims, members of the public and organisations. But I sometimes worry there is a lack of awareness of what...","categories": [],
-        "tags": [],
-        "url": "http://www.philbennett.me/modern-slavery-helplines",
-        "teaser":null},{
         "title": "Technologists: be a good volunteer",
         "excerpt":"As a technologist you’re probably acutely aware of the power and potential for technologies to help organisations transform and make them more impactful. As a would-be volunteer you’ve become passionate about “giving back” and want to use your valuable skills in a meaningful way. Surprisingly, the path to successfully volunteering...","categories": [],
         "tags": [],
