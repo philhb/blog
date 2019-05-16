1e4b987f00d8e870667a92e153a3c26484ca494d
Phil Bennett <phil.bennett@outlook.com>
2019-05-16 21:48:50 +0000

    Deploy from Forestry.io

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -9,6 +9,11 @@ var store = [{
         "tags": [],
         "url": "http://www.philbennett.me/volunteer-technologists",
         "teaser":null},{
+        "title": "National modern slavery helplines: what's involved?",
+        "excerpt":"Over the last few years I’ve come to understand the importance of a national modern slavery helpline. There are now several across the world and they’ve become vital services for potential victims, members of the public and organisations. But I sometimes worry there is a lack of awareness of what...","categories": [],
+        "tags": [],
+        "url": "http://www.philbennett.me/modern-slavery-helplines",
+        "teaser":null},{
         "title": "Moonshot: an approach to cascade ethical behaviour in supply chains",
         "excerpt":"I label some of my ideas as “moonshots”. In my mind, moonshots might be difficult but potentially enormously impactful ideas. Of course, there is a risk of being perceived as naive or ludicrously optimistic by the establishment. But I think it’s worth the reputational risk of sharing an ambitious idea...","categories": [],
         "tags": [],
